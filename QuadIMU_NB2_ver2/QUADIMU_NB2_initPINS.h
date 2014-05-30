34bee018b7a940c9835c3fde45a240767f3fcd45
Scott <sharper9@mix.wvu.edu>
2014-05-30 15:31:07 -0400

    addition of Quad IMU Netburner projects

---
+++ QuadIMU_NB2_ver2/QUADIMU_NB2_initPINS.h
@@ -0,0 +1,26 @@
+/*
+ * QUADIMU_NB2_initPINS.h
+ *
+ *  Created on: May 11, 2014
+ *      Author:Tanmay
+ */
+
+#ifndef QUADIMU_NB2_INITPINS_H_
+#define QUADIMU_NB2_INITPINS_H_
+
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void initPINS();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* QUADIMU_NB2_INITPINS_H_ */
